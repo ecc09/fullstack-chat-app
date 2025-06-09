7c3e60bd67389377d072118b84c6194f53ee5340
ecc09 <tgetwrk@gmail.com>
2025-06-09 15:31:55 +0300

    initial commit

---
+++ backend/src/lib/socket.js
@@ -0,0 +1,37 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server (server, {
+    cors: {
+        origin: ["http://localhost:5173"],
+    },
+});
+
+export function getReceiverSocketId(userId) {
+    return userSocketMap[userId];
+}
+// stores online users
+const userSocketMap = {}
+
+
+io.on("connection", (socket) => {
+    console.log("A user connected", socket.id);
+
+    const userId = socket.handshake.query.userId;
+
+    if (userId) userSocketMap[userId] = socket.id;
+
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+        console.log("A user disconnected", socket.id);
+    });
+});
+
+export { io, app, server}
